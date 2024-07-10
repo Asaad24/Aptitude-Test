07fb7bdf0919e92a41f14fd83a85c1551b8029f9
ASAAD SIDDIQUI <136561207+Asaad24@users.noreply.github.com>
2024-07-10 03:43:59 -0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,9 @@
+signUpButton.addEventListener('click',function(){
+    signIn.style.display="none";
+    signup.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signIn.style.display="block";
+    signup.style.display="none";
+})
+
